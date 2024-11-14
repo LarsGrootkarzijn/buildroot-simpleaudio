21ae4ee6b4ac0d88ca577f3f466412cc3de399d0
Lars Grootkarzijn <larsgrootkarzijnn@gmail.com>
2024-11-14 22:11:36 +0100

    Changes for correct build structure

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
+cd ./buildroot-simpleaudio
 source ./secrets
 
 # Clone Simple Audio HifiberryOS external configuration only if the folder doesn't exist
 if [ ! -d "./hifiberry-os-simpleaudio" ]; then
     echo "Cloning Simple Audio HifiberryOS external configuration"
-    git clone https://github.com/LarsGrootkarzijn/hifiberrys-os-simpleaudio.git
+    git clone https://github.com/LarsGrootkarzijn/hifiberry-os-simpleaudio.git
+fi
+
+if [ ! -d "./outpit" ]; then
+    echo "Create output directory"
+    mkdir ./output
 fi
 
 # Run get-buildroot only if the buildroot folder does not exist
@@ -26,7 +31,7 @@ if [ ! -d "./ti-sdk" ]; then
 	echo "Getting TI-SDK"
 	mkdir ./ti-sdk
 	cd ./ti-sdk
-	wget https://dr-download.ti.com/software-development/software-development-kit-sdk/MD-1BUptXj3op/09.01.00.001/ti-processor-sdk-linux-am335x-evm-09.01.00.001-Linux-x86-Install.bin
+	wget https://dr-download.ti.com/software-development/software-development-kit-sdk/MD-1BUptXj3op/09.01.00.001/ti-processor-sdk-linux-am335x-evm-09.01.00.001-Linux-x86-Install.bin 
 	chmod a+x ./ti-processor-sdk-linux-am335x-evm-09.01.00.001-Linux-x86-Install.bin
 	./ti-processor-sdk-linux-am335x-evm-09.01.00.001-Linux-x86-Install.bin --prefix ./ --mode unattended
 	cd ..
@@ -35,6 +40,10 @@ fi
 echo "Set Simple Audio Kernel config"
 cp ./configs/kernel/.config ./ti-sdk/board-support/ti-linux-kernel-6.1.46+gitAUTOINC+1d4b5da681-g1d4b5da681/
 
+echo "Placing Roomplayer DTS in DTS boot folder."
+cp ./device-trees/kernel/roomplayer.dts ./ti-sdk/board-support/ti-linux-kernel-6.1.46+gitAUTOINC+1d4b5da681-g1d4b5da681/arch/arm/boot/dts/
+
 echo "Set Simple Audio Buildroot config"
 
 cp ./configs/buildroot/.config ./buildroot/
+
