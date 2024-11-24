3fdd7fcf6c6102ab44402b390cffcbeac98a650d
Lars Grootkarzijn <larsgrootkarzijnn@gmail.com>
2024-11-24 16:35:13 +0100

    Update update script

--- upgrade-roomplayer.sh
+++ upgrade-roomplayer.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
+download_file() {
+  if command -v curl &> /dev/null; then
+    curl -L -o "$2" "$1"
+  elif command -v wget &> /dev/null; then
+    wget -qO "$2" "$1"
+  else
+    echo "Neither curl nor wget are available for downloading."
+    exit 1
+  fi
+}
 
-# Get the latest release tag
-LATEST_TAG=$(wget -qO- https://api.github.com/repos/LarsGrootkarzijn/buildroot-simpleaudio/releases/latest | grep '"tag_name":' | cut -d '"' -f 4) || { echo "Failed to fetch the latest release tag."; exit 1; }
+LATEST_TAG=$(download_file "https://api.github.com/repos/LarsGrootkarzijn/buildroot-simpleaudio/releases/latest" -) 
+LATEST_TAG=$(echo "$LATEST_TAG" | grep '"tag_name":' | cut -d '"' -f 4) || { echo "Failed to fetch the latest release tag."; exit 1; }
 
-# Construct URLs for the files
 UIMAGE_URL="https://github.com/LarsGrootkarzijn/buildroot-simpleaudio/releases/download/${LATEST_TAG}/uImage"
 
-if ! wget $UIMAGE_URL; then
-  echo "Error downloading uImage."
-  exit 1
-fi
+echo "Downloading $LATEST_TAG"
+
+download_file "$UIMAGE_URL" "uImage"
 
 if [ ! -d /boot ]; then
   echo "Creating /boot directory..."
@@ -17,6 +24,7 @@ if [ ! -d /boot ]; then
 fi
 
 BLK=$(cat /proc/cmdline | grep -o 'root=/dev/mmcblk[0-9]' | cut -d '=' -f 2)
+echo "Device to write to is $BLK"
 mount ${BLK}p1 /boot
 
 rm /boot/uImage
@@ -25,5 +33,6 @@ rm /boot/bootargs
 
 echo "console=ttyS0,115200 root=/dev/ram0 rw rdinit=/sbin/init" >> /boot/bootargs
 
+umount ${BLK}p1
 sync 
 reboot
