6baf67e2da35b001ab23044091778b3de504aa98
Lars Grootkarzijn <larsgrootkarzijnn@gmail.com>
2025-01-03 15:23:32 +0100

    Add full build, changes to other build scripts

--- upgrade-roomplayer.sh
+++ upgrade-roomplayer.sh
@@ -2,14 +2,14 @@ download_file() {
   if command -v curl &> /dev/null; then
     curl -s -L -o "$2" "$1"
     if [ $? -ne 0 ]; then
-      echo "curl failed to download the file."
-      exit 1
+
+      return 1
     fi
   elif command -v wget &> /dev/null; then
     wget -qO "$2" "$1"
     if [ $? -ne 0 ]; then
-      echo "wget failed to download the file."
-      exit 1
+
+      return 1
     fi
   else
     echo "Neither curl nor wget are available for downloading."
@@ -54,12 +54,29 @@ case "$answer" in
 esac
 
 LATEST_TAG=$(download_file "https://api.github.com/repos/LarsGrootkarzijn/buildroot-simpleaudio/releases/latest" -)
+
+if [ -z "$LATEST_TAG" ]; then
+    echo "LATEST_TAG is empty."
+else
+    echo "LATEST_TAG is not empty: $LATEST_TAG"
+fi
+
+if [ -z "$LATEST_TAG" ]; then
+	LATEST_TAG=$(download_file "http://larsgrootkarzijn.nl/version.php" -)
+fi
+
+if [ -z "$LATEST_TAG" ]; then
+    echo "Failed to fetch the latest release tag."
+    exit 1
+fi
+
 LATEST_TAG=$(echo "$LATEST_TAG" | grep '"tag_name":' | cut -d '"' -f 4) || { echo "Failed to fetch the latest release tag."; exit 1; }
 
-echo "Found version ${LATEST_TAG}" 
+echo "Found version ${LATEST_TAG}"
 echo
 
 UIMAGE_URL="https://github.com/LarsGrootkarzijn/buildroot-simpleaudio/releases/download/${LATEST_TAG}/uImage_Install"
+UIMAGE_URL_BACKUP="http://larsgrootkarzijn.nl/uimage.php?tag=${LATEST_TAG}"
 
 echo "Downloading $LATEST_TAG"
 echo
@@ -67,9 +84,23 @@ echo
 download_file "$UIMAGE_URL" "uImage"
 
 if [ ! -s "uImage" ]; then
+    echo "Error occurred during file download. Trying larsgrootkarzijn.nl..."
+
+    download_file "$UIMAGE_URL_BACKUP" "uImage"
+
+    # Check if backup download fails
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to download from the backup URL as well."
+        exit 1
+    fi
+fi
+
+if [ ! -s "uImage" ]; then
     echo "Download failed."
     exit 1
 fi
+
+echo "Succesfully Downloaded uImage"
 
 if [ ! -d /boot ]; then
   echo "Creating /boot directory..."
@@ -79,22 +110,15 @@ fi
 MMC_DEVICE=$(ls /dev/mmcblk* | grep -o 'mmcblk[0-9]\+' | head -n 1)
 
 # Extract the 'X' part from 'mmcblkX'
-if [[ $MMC_DEVICE =~ ^mmcblk([a-zA-Z0-9]+)$ ]]; then
-    X=$(echo $MMC_DEVICE | sed 's/mmcblk//')
-else
-    echo "Invalid MMC_DEVICE format: $MMC_DEVICE"
-    exit 1
-fi
+X=$(echo "$MMC_DEVICE" | cut -d 'k' -f 2)
 
 echo "Found MMC device: $MMC_DEVICE"
-
 mount /dev/${MMC_DEVICE}p1 /boot
 
-if ! mountpoint -q /boot; then
+if ! grep -q ' /boot ' /proc/mounts; then
     echo "Failed to mount /boot."
     exit 1
 fi
-
 
 if ! rm /boot/uImage; then
     echo "Failed to remove /boot/uImage."
@@ -126,9 +150,10 @@ echo
 echo "All ready, will reboot to upgrade, Press any key to cancel reboot."
 echo
 
-COUNTDOWN=5
+COUNTDOWN=10
+i=$COUNTDOWN
 
-for ((i=$COUNTDOWN; i>=1; i--)); do
+for i in $(seq $COUNTDOWN -1 1); do
     echo -ne "Rebooting in $i... \r"
     if read -t 1 -n 1; then
         echo -e "Reboot canceled."
